59f72a6177faaf38baf8e228c880480829449f4f
prithvirajktagadinamani@gmail.com <prithvirajktagadinamani@gmail.com>
2021-10-02 18:23:19 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react'
+import {BrowserRouter, Route, Switch} from "react-router-dom"
+import Home from "./Components/Home"
+import About from "./Components/About"
+import Contact from "./Components/Contact"
+import Portfolio from "./Components/Portfolio"
+import NotFound from "./Components/NotFound"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+      <Switch>
+        <Route exact path="/home" component={Home}/>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/about" component={About}/>
+        <Route exact path="/contact" component={Contact}/>
+        <Route exact path="/portfolio" component={Portfolio}/>
+        <Route component={NotFound}/>
+      </Switch>
+      </BrowserRouter>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/Components/Portfolio.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Portfolio() {
+    return (
+        <div>
+            Portfolio page
+        </div>
+    )
+}
+
+export default Portfolio
